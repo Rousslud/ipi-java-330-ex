d29f39a2325729fe2a8cee31a6c2ac4855f83686
rousslud <rousselot.ludovic@yahoo.fr>
2018-03-03 08:57:14 +0100

    Exercises 5-6-7: add 'consumes' and 'produces' to the commercial controller

--- src/main/java/com/ipiecoles/java/java330/controller/CommercialController.java
+++ src/main/java/com/ipiecoles/java/java330/controller/CommercialController.java
@@ -2,7 +2,10 @@ package com.ipiecoles.java.java330.controller;
 
 import com.ipiecoles.java.java330.service.CommercialService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,9 +19,11 @@ public class CommercialController {
 
     @RequestMapping(
             value = "/{id}",
-            method = RequestMethod.POST
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = MediaType.TEXT_HTML_VALUE
     )
-    public String enregistre(@RequestParam("id") Long id) {
+    public String enregistre(@RequestParam("id") Long id, @RequestBody MultiValueMap<String,String> map) {
 
         return null;
     }
