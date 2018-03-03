1da90ca6f5f6e6a4118ffafc50a579b838eb9ac6
rousslud <rousselot.ludovic@yahoo.fr>
2018-03-03 09:17:24 +0100

    Exercises 5-6-7: redirecting the page when saving changes

--- src/main/java/com/ipiecoles/java/java330/controller/CommercialController.java
+++ src/main/java/com/ipiecoles/java/java330/controller/CommercialController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/commercials")
@@ -25,9 +26,9 @@ public class CommercialController {
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE
     )
-    public String enregistre(@PathVariable("id") Long id, Commercial employe, Map<String, Object> model, RedirectAttributes attributes) {
+    public RedirectView enregistre(@PathVariable("id") Long id, Commercial employe, Map<String, Object> model, RedirectAttributes attributes) {
     	employe = this.commercialService.updateEmploye(id, employe);
     	model.put("employe", employe);
-    	return "employes/detail";
+    	return new RedirectView("/employes/" + id);
     }
 }
